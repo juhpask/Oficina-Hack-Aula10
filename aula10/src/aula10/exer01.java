30fe3b2a0f027ffa0ad3c84a1d5bbe8f7456ea98
Julia Paskauskas <ravenakaus@gmail.com>
2021-11-02 20:46:18 -0300

    Criação do pacote e exercução do exercício 1

---
+++ aula10/src/aula10/exer01.java
@@ -0,0 +1,30 @@
+
+package aula10;
+
+import java.util.Scanner;
+public class exer01 {
+
+    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        int num1;
+        int num2;
+        
+        System.out.println("Entre com o primeiro número:");
+        num1 = scan.nextInt();
+        System.out.println("Entre com o segundo número:");
+        num2 = scan.nextInt();
+        
+        if (num1==num2){
+            System.out.println("Os números são iguais.");
+        } else if (num1>num2){
+            System.out.println(num1 + " é maior que " + num2);
+        } else {
+            System.out.println(num2 + " é maior que " + num1);
+        }
+        
+        scan.close();
+    }
+    
+}
